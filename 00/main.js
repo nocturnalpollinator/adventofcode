532f9dea325a370eb622212064c5ccb75fb40427
Robin <hejrobinjonsson@gmail.com>
2022-11-28 15:40:46 +0100

    Preparations

---
+++ 00/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+    run: (glob) => {
+        const n = glob.fsn
+	const pt1 = 1
+	const pt2 = 2
+
+        return { result: `Part I: ${pt1}\nPart II: ${pt2}` }
+    }
+}
---
+++ createday.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir $1;
+cp 00/* $1;
