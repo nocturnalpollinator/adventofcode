6d6f4ddddf8dcb65d7b92f8b92bca7b2fc3a0c90
Robin <hejrobinjonsson@gmail.com>
2022-12-02 07:03:28 +0100

    Day 2 tidy

--- 02/main.js
+++ 02/main.js
@@ -6,53 +6,29 @@ module.exports = {
             A: {
                 X: 4,
                 Y: 8,
-                Z: 3,
-                win: 'Y',
-                draw: 'X',
-                lose: 'Z'
+                Z: 3
             },
             // Paper
             B: {
                 X: 1,
                 Y: 5,
-                Z: 9,
-                win: 'Z',
-                draw: 'Y',
-                lose: 'X'
+                Z: 9
             },
-            // S
+            // Scissors
             C: {
                 X: 7,
                 Y: 2,
-                Z: 6,
-                win: 'X',
-                draw: 'Z',
-                lose: 'Y'
+                Z: 6
             }
         }
 
-        var count = 0;
-        var count2 = 0;
+        const count = n.reduce((acc, c) => c.length > 0 ? acc + strat[c[0]][c[2]] : acc, 0)
 
-        n.forEach((element, i) => {
-            if (element !== '') {
-                x = element.split(' ') 
-                count += strat[x[0]][x[1]]
-                if (x[1] == 'X') {
-                    // lost
-                    count2 += parseInt(strat[x[0]][strat[x[0]].lose])
-                }
-                if (x[1] == 'Y') {
-                    // draw
-                    count2 += parseInt(strat[x[0]][strat[x[0]].draw])
-                }
-                if (x[1] == 'Z') {
-                    // win
-                    count2 += parseInt(strat[x[0]][strat[x[0]].win])
-                }
-            }
-        });
-
+        const count2 = n.reduce((acc, c) => c.length > 0 ? (
+            c[2] == 'X' ? acc + Object.values(strat[c[0]]).filter(x => x < 4).pop() :
+            c[2] == 'Y' ? acc + Object.values(strat[c[0]]).filter(x => x > 3 && x < 7).pop() :
+            acc + Object.values(strat[c[0]]).filter(x => x > 6).pop()
+        ) : acc, 0)
 
         const pt1 = count
         const pt2 = count2
