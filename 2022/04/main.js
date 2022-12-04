7b104c7265380bef144a77994b14aaa77c8957ed
Robin <hejrobinjonsson@gmail.com>
2022-12-04 08:58:25 +0100

    Day 04

--- 2022/04/main.js
+++ 2022/04/main.js
@@ -1,40 +1,15 @@
-const { text } = require("stream/consumers")
-
 module.exports = {
     run: (helpers) => {
         const n = helpers.fsn
 
-        const test = `2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8`.split('\n')
-
-        const p = []
         const a = n.map(x => {
             const q = x.split(',')
             const w = [q[0].split('-').map(x => parseInt(x)), q[1].split('-').map(x => parseInt(x))]
-            const e = w.map(x => {
-                const string = []
-                for (var i = x[0]; i < x[1] + 1; i++) {
-                    string.push(i)
-                }
-                return string
-            })
-            const join = e.map(x => x.map(x => 'x' + x + 'x').join(''))
-            p.push(e)
-            return join[0].includes(join[1]) || join[1].includes([join[0]])
+            return w.map(x => Array.from({ length: x[1] - x[0] + 1 }, (e, i) => i + x[0]))
         }).filter(x => x)
-       
 
-        const pt1 = a.length
-         const pt2 = p.filter(x => {
-            for(var i = x[0][0]; i < x[0].last + 1; i++) {
-                    if (x[1].includes(i)) return true
-            }
-            return false 
-        }).length
+        const pt1 = a.map(x => x.map(x => x.map(x => 'x' + x + 'x').join(''))).filter(x => x[0].includes(x[1]) || x[1].includes(x[0])).length
+        const pt2 = a.filter(x => x[1].includes(x[0][0]) || x[1].includes(x[0].last) || x[0].includes(x[1][0]) || x[0].includes(x[1].last)).length
 
         return { result: `Part I: ${pt1}\nPart II: ${pt2}` }
     }
