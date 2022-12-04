6a9d7298825effd7ad8f08d81e7fc6e586ab9448
Robin <hejrobinjonsson@gmail.com>
2022-12-04 09:25:11 +0100

    *Schnip schnip*

--- 2022/04/main.js
+++ 2022/04/main.js
@@ -1,9 +1,8 @@
 module.exports = {
     run: (helpers) => {
-        const n = helpers.fsn
-        const a = n.map(x => x.split(',').map(x => x.split('-').arrToInt()).map(x => Array.fromTo(x[0], x[1]))).filter(x => x)
-        const pt1 = a.map(x => x.map(x => x.map(x => 'x' + x + 'x').join(''))).filter(x => x[0].includes(x[1]) || x[1].includes(x[0])).length
-        const pt2 = a.filter(x => x[0].intersects(x[1])).length
+        const n = helpers.fsn.map(x => x.split(',').map(x => x.split('-').arrToInt()).map(x => Array.fromTo(x[0], x[1]))).filter(x => x)
+        const pt1 = n.map(x => x.map(x => x.map(x => 'x' + x + 'x').join(''))).filter(x => x[0].includes(x[1]) || x[1].includes(x[0])).length
+        const pt2 = n.filter(x => x[0].intersects(x[1])).length
         return { result: `Part I: ${pt1}\nPart II: ${pt2}` }
     }
 }
