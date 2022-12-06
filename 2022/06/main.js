00c09d63e1461eab607c5e506c29055c2b7ba17b
Robin <hejrobinjonsson@gmail.com>
2022-12-06 16:21:18 +0100

    Early ejection in reduce

--- 2022/06/main.js
+++ 2022/06/main.js
@@ -1,10 +1,12 @@
 module.exports = {
     run: (helpers) => {
         const n = helpers.file.split('')
-        const unique = (acc, x, i, length) => {
-            if (!Array.isArray(acc)) return acc
-
-            if (acc.length == length && acc.uniq().length == length) return i
+        const unique = (acc, x, i, length, arr) => {
+            if (acc.length == length && acc.uniq().length == length) {
+                // Eject reduce early
+                arr.splice(1)
+                return i
+            }
 
             if (acc.length == length) acc.shift()
 
@@ -12,8 +14,8 @@ module.exports = {
             return acc
         }
 
-        const pt1 = n.reduce((acc, x, i) => unique(acc, x, i, 4), [])
-        const pt2 = n.reduce((acc, x, i) => unique(acc, x, i, 14), [])
+        const pt1 = n.slice(0).reduce((acc, x, i, arr) => unique(acc, x, i, 4, arr), [])
+        const pt2 = n.slice(0).reduce((acc, x, i, arr) => unique(acc, x, i, 14, arr), [])
         
         return { result: `Part I: ${pt1}\nPart II: ${pt2}` }
     }
